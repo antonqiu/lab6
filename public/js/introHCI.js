995b22bcd0b1d9e6d79b7a928b3dfe330c62a9e9
Anton Qiu <qiuchangtong@gmail.com>
2016-02-09 03:05:29 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,32 +1,43 @@
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
+$(document).ready(function () {
+    initializePage();
 })
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
-
-	$('#colorBtn').click(randomizeColors);
+    $('.project a').click(addProjectDetails);
+    $('#colorBtn').click(randomizeColors);
 }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
-	// Prevent following the link
-	e.preventDefault();
+    // Prevent following the link
+    e.preventDefault();
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+    // Get the div ID, e.g., "project3"
+    var projectID = $(this).closest('.project').attr('id');
+    // get rid of 'project' from the front of the id 'project3'
+    var idNumber = projectID.substr('project'.length);
+    // get call
+    console.log("Calling " + "/project/" + idNumber);
+    $.get("/project/" + idNumber, getProjectJSON);
+    console.log("User clicked on project " + idNumber);
+}
 
-	console.log("User clicked on project " + idNumber);
+function getProjectJSON(result) {
+    console.log(result);
+    var projectHTML = '<img src="' + result['image'] +
+        '" class="detailsImage">' +
+        '<p><small>' + result['title'] + '</small></p>' +
+        '<p><small>' + result['date'] + '</small></p>' +
+        '<p>' + result['summary'] + '</p>';
+    $("#project" + result['id'] + " .details").html(projectHTML);
 }
 
 /*
@@ -34,5 +45,16 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
-	console.log("User clicked on color button");
+    console.log("User clicked on color button");
+    $.get('/palette', setColor);
+}
+
+function setColor(result) {
+    console.log(result);
+    var colors = result['colors']['hex'];
+    $('body').css('background-color', colors[0]);
+    $('.thumbnail').css('background-color', colors[1]);
+    $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+    $('p').css('color', colors[3]);
+    $('.project img').css('opacity', .75);
 }
